032811af06fc4c601fcebd2478e4530fe4d5456a
Corey-33 <cdki224@g.uky.edu>
2017-04-20 19:54:38 -0400

    Starting to implement register, as well as changed email.username variables on login.pug

--- dbconnection.js
+++ dbconnection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var fs = require('fs');
-var config = '/Users/Natsagaa/Documents/UK/CS405/project/config';
+var config = 'config.txt';
 var host;
 var user;
 var pswd;
@@ -8,15 +8,15 @@ var db;
 
 var databaseConfig = fs.readFileSync(config, 'utf8');
 
-databaseConfig = databaseConfig.split('\n');
+databaseConfig = databaseConfig.split(',');
 
 var pool = mysql.createPool({
     connectionLimit : 100, //important
-    host     : databaseConfig[0],
-    user     : databaseConfig[1],
-    password : databaseConfig[2],
-    database : databaseConfig[3],
-    debug    :  false
+	host: databaseConfig[0],
+	user: databaseConfig[1],
+	password: databaseConfig[2],
+	database: databaseConfig[3]
+//    debug    :  false
 });
 
 module.exports = pool;
--- routes/register.js
+++ routes/register.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var pool = require('../dbconnection');
 var nodemailer = require('nodemailer');
 
 /* GET contact . */
 router.get('/', function(req, res, next) {
+  console.log(req.body);
+  console.log('type');
+  res.render('register', {title: 'This is title for GET method.'});
   res.render('register', {title: 'Registration page'});
 });
 
 /* GET contact . */
 router.post('/', function(req, res, next) {
+    console.log(req.body);
+
+  query = 'SELECT * FROM user WHERE email = "'+req.body['lg_username']+
+          '" and password = "'+req.body['lg_password']+'" LIMIT 1';
+
+
   res.render('register', {title: 'Registration page'});
 });
 
